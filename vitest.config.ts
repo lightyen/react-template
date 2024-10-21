233e26bdfe2291afa3f09aa8b0fd45808448d107
lightyen <lightyen0123@gmail.com>
2024-10-21 21:55:01 +0800

    vitest

---
+++ vitest.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from "vitest/config"
+
+export default defineConfig({
+	test: {
+		// ...
+	},
+})
+
+// const reactConfig = {
+// 	plugins: [react()],
+// 	test: {
+// 		browser: {
+// 			enabled: true,
+// 			name: "chromium",
+// 			provider: "playwright",
+// 		},
+// 	},
+// }
