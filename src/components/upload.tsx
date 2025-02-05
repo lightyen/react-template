aaff25333a98e8b9c4b1d2cd0c5f2644127c7b81
lightyen <lightyen0123@gmail.com>
2025-02-05 16:49:34 +0800

    update

--- src/components/upload.tsx
+++ src/components/upload.tsx
@@ -1,117 +1,83 @@
-import { useCallback, useRef, useState } from "react"
-import { FormattedMessage } from "react-intl"
+import { useRef } from "react"
 import { composeRefs } from "./lib"
 
-interface UploadProps extends React.InputHTMLAttributes<HTMLInputElement> {
+export interface UploadHandler {
+	onFiles?: (files: FileList | null) => void
+}
+
+export interface DropareaProps extends UploadHandler, React.HTMLAttributes<HTMLDivElement> {
+	ref?: React.Ref<HTMLDivElement>
+}
+
+interface UseDragDropReturn<T> {
+	register(options?: { ref?: React.Ref<T>; overAttr?: string }): {
+		ref(node: T): void
+		onDragOver(e: React.DragEvent<T>): void
+		onDrop(e: React.DragEvent<T>): void
+		onDragLeave(e: React.DragEvent<T>): void
+	}
+}
+
+/**
+ * @example
+ *
+ * ```tsx
+ * // useDragDrop()
+ * // the "data-over" attribute
+ *
+ * function Component() {
+ * 	const { register } = useDragDrop<HTMLDivElement>({ onFiles })
+ * 	return <div {...register({ ref, overAttr })} />
+ * }
+ * ```
+ */
+export function useDragDrop<E extends HTMLElement>({ onFiles }: UploadHandler = {}): UseDragDropReturn<E> {
+	const ref = useRef<E>(null)
+	return {
+		register({ ref: customRef, overAttr = "data-over" } = {}) {
+			return {
+				ref: composeRefs(ref, customRef),
+				onDragOver(e) {
+					e.preventDefault()
+					e.stopPropagation()
+					if (ref.current) {
+						ref.current.setAttribute(overAttr, "")
+					}
+				},
+				onDrop(e) {
+					e.preventDefault()
+					e.stopPropagation()
+					if (ref.current) {
+						ref.current.removeAttribute(overAttr)
+					}
+					onFiles?.(e.dataTransfer.files)
+				},
+				onDragLeave(e) {
+					e.preventDefault()
+					e.stopPropagation()
+					if (ref.current) {
+						ref.current.removeAttribute(overAttr)
+					}
+				},
+			}
+		},
+	}
+}
+
+export interface InputFileProps
+	extends UploadHandler,
+		Omit<React.InputHTMLAttributes<HTMLInputElement>, "type" | "onChange"> {
 	ref?: React.Ref<HTMLInputElement>
 }
 
-export function MdiCloudUpload(props: React.SVGProps<SVGSVGElement>) {
+export function InputFile({ onFiles, ...props }: InputFileProps) {
 	return (
-		<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" {...props}>
-			<path
-				fill="currentColor"
-				d="M11 20H6.5q-2.28 0-3.89-1.57Q1 16.85 1 14.58q0-1.95 1.17-3.48q1.18-1.53 3.08-1.95q.63-2.3 2.5-3.72Q9.63 4 12 4q2.93 0 4.96 2.04Q19 8.07 19 11q1.73.2 2.86 1.5q1.14 1.28 1.14 3q0 1.88-1.31 3.19T18.5 20H13v-7.15l1.6 1.55L16 13l-4-4l-4 4l1.4 1.4l1.6-1.55Z"
-			></path>
-		</svg>
-	)
-}
-
-export function MdiFile(props: React.SVGProps<SVGSVGElement>) {
-	return (
-		<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" {...props}>
-			<path
-				fill="currentColor"
-				d="M13 9V3.5L18.5 9M6 2c-1.11 0-2 .89-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8l-6-6z"
-			></path>
-		</svg>
-	)
-}
-
-export function Upload({ type: _, onChange, className, style, ref, ...props }: UploadProps) {
-	const labelRef = useRef<HTMLLabelElement>(null)
-	const inputRef = useRef<HTMLInputElement>(null)
-	const [name, setName] = useState("")
-	const onFiles = useCallback((files: FileList | null) => {
-		if (!labelRef.current || !inputRef.current) {
-			return
-		}
-
-		const length = files?.length ?? 0
-
-		if (inputRef.current.files?.length != length) {
-			inputRef.current.files = files
-		}
-
-		const s: string[] = []
-		if (files) {
-			for (const f of files) {
-				s.push(f.name)
-			}
-		}
-
-		if (s.length) {
-			labelRef.current.setAttribute("data-file", "")
-		} else {
-			labelRef.current.removeAttribute("data-file")
-		}
-		setName(s.join("\n"))
-	}, [])
-	return (
-		<label
-			tw="relative select-none box-border flex items-center justify-center rounded-xl
-			border-2 border-solid border-input transition-colors
-			[&[data-file]]:bg-primary/20
-			hover:(
-				bg-input/20
-				[&[data-file]]:(border-primary bg-primary/15)
-			)
-			[&[data-over]]:(border-primary bg-primary/15 border-dashed)
-			"
-			ref={labelRef}
-			className={className}
-			style={style}
-			onDragOver={e => {
-				e.preventDefault()
-				e.stopPropagation()
-				if (labelRef.current) {
-					labelRef.current.setAttribute("data-over", "")
-				}
+		<input
+			type="file"
+			onChange={e => {
+				onFiles?.(e.target.files)
 			}}
-			onDrop={e => {
-				e.preventDefault()
-				e.stopPropagation()
-				if (labelRef.current) {
-					labelRef.current.removeAttribute("data-over")
-				}
-				onFiles(e.dataTransfer.files)
-			}}
-			onDragLeave={e => {
-				e.preventDefault()
-				e.stopPropagation()
-				if (labelRef.current) {
-					labelRef.current.removeAttribute("data-over")
-				}
-			}}
-		>
-			<input
-				type="file"
-				tw="absolute w-0 h-0"
-				ref={composeRefs(ref, inputRef)}
-				onChange={e => {
-					onFiles(e.target.files)
-					onChange?.(e)
-				}}
-				{...props}
-			/>
-			<div tw="h-28 w-full flex flex-col items-center justify-center gap-0.5 px-2">
-				<div tw="flex justify-center">
-					{name ? <MdiFile width={32} height={32} /> : <MdiCloudUpload width={32} height={32} />}
-				</div>
-				<div tw="text-xl text-center text-balance">
-					<p>{name || <FormattedMessage id="upload_file" />}</p>
-				</div>
-			</div>
-		</label>
+			{...props}
+		/>
 	)
 }
--- src/pages/Form/FormPage.tsx
+++ src/pages/Form/FormPage.tsx
@@ -1,6 +1,7 @@
 import { useVirtualizer } from "@tanstack/react-virtual"
-import { startTransition, useEffect, useId, useMemo, useRef, useState } from "react"
+import { startTransition, useCallback, useEffect, useId, useMemo, useRef, useState } from "react"
 import { FormProvider, useForm, useFormContext } from "react-hook-form"
+import { FormattedMessage } from "react-intl"
 import { tw } from "twobj"
 import { Button } from "~/components/button"
 import { Checkbox, CheckboxTree, type CheckboxTreeNode } from "~/components/checkbox"
@@ -11,7 +12,7 @@ import { Label } from "~/components/label"
 import { Popover, PopoverClose, PopoverContent, PopoverTrigger } from "~/components/popover"
 import { SliderRange, SliderRoot, SliderThumb, SliderTrack } from "~/components/slider"
 import { Switch } from "~/components/switch"
-import { Upload } from "~/components/upload"
+import { InputFile, useDragDrop } from "~/components/upload"
 import { addresses } from "~/data/macaddr"
 import { zonenames } from "~/data/zonename"
 import { Header, Separator } from "~/pages/common"
@@ -513,5 +514,87 @@ function FormDetail() {
 				{...methods.register("mac", { validate: value => parseMAC(value) != null })}
 			/>
 		</>
+	)
+}
+
+function MdiCloudUpload(props: React.SVGProps<SVGSVGElement>) {
+	return (
+		<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" {...props}>
+			<path
+				fill="currentColor"
+				d="M11 20H6.5q-2.28 0-3.89-1.57Q1 16.85 1 14.58q0-1.95 1.17-3.48q1.18-1.53 3.08-1.95q.63-2.3 2.5-3.72Q9.63 4 12 4q2.93 0 4.96 2.04Q19 8.07 19 11q1.73.2 2.86 1.5q1.14 1.28 1.14 3q0 1.88-1.31 3.19T18.5 20H13v-7.15l1.6 1.55L16 13l-4-4l-4 4l1.4 1.4l1.6-1.55Z"
+			></path>
+		</svg>
+	)
+}
+
+function MdiFile(props: React.SVGProps<SVGSVGElement>) {
+	return (
+		<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" {...props}>
+			<path
+				fill="currentColor"
+				d="M13 9V3.5L18.5 9M6 2c-1.11 0-2 .89-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8l-6-6z"
+			></path>
+		</svg>
+	)
+}
+
+function Upload() {
+	const ref = useRef<HTMLLabelElement>(null)
+	const inputRef = useRef<HTMLInputElement>(null)
+
+	const [name, setName] = useState("")
+
+	const onFiles = useCallback((files: FileList | null) => {
+		if (!ref.current || !inputRef.current) {
+			return
+		}
+
+		const length = files?.length ?? 0
+
+		if (inputRef.current.files?.length != length) {
+			inputRef.current.files = files
+		}
+
+		const s: string[] = []
+		if (files) {
+			for (const f of files) {
+				s.push(f.name)
+			}
+		}
+
+		if (s.length) {
+			ref.current.setAttribute("data-file", "")
+		} else {
+			ref.current.removeAttribute("data-file")
+		}
+		setName(s.join("\n"))
+	}, [])
+
+	const { register } = useDragDrop<HTMLLabelElement>({ onFiles })
+
+	return (
+		<label
+			tw="relative select-none box-border flex items-center justify-center rounded-xl
+				border-2 border-solid border-input transition-colors
+				[&[data-file]]:bg-primary/20
+				hover:(
+					bg-input/20
+					[&[data-file]]:(border-primary bg-primary/15)
+				)
+				[&[data-over]]:(border-primary bg-primary/15 border-dashed)
+			"
+			{...register({ ref })}
+		>
+			<InputFile tw="absolute w-0 h-0" ref={inputRef} onFiles={onFiles} />
+			<div tw="h-28 w-full flex flex-col items-center justify-center gap-0.5 px-2">
+				<div tw="flex justify-center">
+					{name ? <MdiFile width={32} height={32} /> : <MdiCloudUpload width={32} height={32} />}
+				</div>
+				<div tw="text-xl text-center text-balance">
+					<p>{name || <FormattedMessage id="upload_file" />}</p>
+				</div>
+			</div>
+		</label>
 	)
 }
